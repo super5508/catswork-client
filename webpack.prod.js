7e2af0e5a54c71506a5d0e2a3dbc01824d239fa0
super5508 <srdev5008@gmail.com>
2019-08-16 19:34:27 +0530

    added build

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const config = require('./webpack.config')
 const prodConfig = Object.assign({}, config, {
 	mode: 'production',
 	output: Object.assign({}, config.output, {
-		path: path.resolve('./prod/public')
+		path: path.resolve('./public')
 	})
 })
 
