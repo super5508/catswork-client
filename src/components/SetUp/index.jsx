32fee356d7c055c6c8092ee5bd1acd40d82b9eb6
super5508 <srdev5008@gmail.com>
2019-08-21 02:18:23 +0530

    client changes from cattrack to catwork

--- src/components/SetUp/index.jsx
+++ src/components/SetUp/index.jsx
@@ -25,7 +25,7 @@ class SetUp extends React.Component {
 					<Heading size={1} primary>Welcome</Heading>
 					<Heading size={2}>Looks like it's your first time here</Heading>
 					<p>We need to get a few things out of the way before you can use the app.</p>
-					<p>The following information will help us to improve your experience using CatsTrack.</p>
+					<p>The following information will help us to improve your experience using CatsWork.</p>
 					<p>If you're unsure for any of these options, just choose what fits best. You'll be able to change this information at any point.</p>
 					<SetUpForm onSetUp={this._onSetUp} />
 				</section>
--- src/components/SignedOut/index.jsx
+++ src/components/SignedOut/index.jsx
@@ -12,7 +12,7 @@ const SignedOut = () => (
 		<Nav />
 		<section className={s.signedOut}>
 			<Heading size={1} primary>Hi there</Heading>
-			<Heading size={2}>Welcome to <span className={s.catsTrack}>CatsTrack</span></Heading>
+			<Heading size={2}>Welcome to <span className={s.catsTrack}>CatsWork</span></Heading>
 			<p>Sign in using your Google account to start using the app.</p>
 			{/*TOFIX <Button size='large' href='/auth/sign-in'>Sign in</Button> */}
 			<Button size='large' href='auth/google'>Sign in</Button>
