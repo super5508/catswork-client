e7dfb4d1a8607564d86f20c4942e277f2930a380
super5508 <srdev5008@gmail.com>
2019-08-25 19:05:00 +0530

    production url error

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 console.log(`Current Environment:`, process.env.NODE_ENV)
 const config = {
 	server: {
-		url: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://dashboard.catswork.io',
+		url: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://dashboard.catswork.io/',
 		graphQLPath: 'graphql'
 	}
 }
