a133a936f87a2b2bc7325d501b1fd174ca44e87d
super5508 <srdev5008@gmail.com>
2019-09-04 00:54:15 +0530

    impletmented scroll

--- src/childComponents/tableCreator.js
+++ src/childComponents/tableCreator.js
@@ -5,21 +5,26 @@ import { fixStringValues  } from "../helperFunction/commonFunctions"
 
 export const GraphTableCreator = (props) => {
   return (
-    <table style={{border: "1px solid black",  textAlign:"center"}}>
-     <thead>
+    <table style={{textAlign:"center"}}>
+     <thead style={{display: "block"}}>
         <tr>
           <th style={{borderBottom: "1px solid black", backgroundColor:"#FF8A65", color: "white", textAlign:"left", padding: 15, width:200}}>{props.tableHeadingKey}</th>
           <th style={{borderBottom: "1px solid black", backgroundColor:"#FF8A65",  color: "white", textAlign:"center", padding: 15, width:100}}>{props.tableHeadingValue}</th> 
         </tr>
       </thead>
+      <tbody style={{
+        display: "block",
+      height: "300px",       
+      overflowY: "auto",    
+      overflowX: "hidden"  
+    }}>
     {props.graphData.map((el, index) => (
-    <tbody>
       <tr>
         <td style={{ textAlign:"left", padding: 15, width:200, backgroundColor: index % 2 === 0 ? 'white' : '#FFCCBC'}}>{fixStringValues(el.x)}</td>
         <td style={{ textAlign:"center", padding: 15, width:100, backgroundColor: index % 2 === 0 ? 'white' : '#FFCCBC'}}>{el.y}</td>
       </tr>
-    </tbody>
     ))}
+    </tbody>
     </table>
   )
 }
